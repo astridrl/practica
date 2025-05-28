8758d23cbb68e962bd7142ad38867e0ef4d91064
[Astrid Ruíz] <[astridruizz05@gmail.com]>
2025-05-28 00:22:20 -0600

    subiendo archivos

---
+++ pruebas/include/iniciando.h
@@ -0,0 +1,16 @@
+#ifndef INICIANDO_H
+#define INICIANDO_H
+
+
+class iniciando
+{
+    public:
+        iniciando();
+        virtual ~iniciando();
+
+    protected:
+
+    private:
+};
+
+#endif // INICIANDO_H
---
+++ pruebas/src/iniciando.cpp
@@ -0,0 +1,13 @@
+#include "iniciando.h"
+
+iniciando::iniciando()
+{
+    //ctor
+}
+
+iniciando::~iniciando()
+{
+    //dtor
+}
+
+//aquí probando
